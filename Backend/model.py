97e07b855255298f665607e2a353fc5f102f207c
siya-43 <siyajulka03@gmail.com>
2026-04-26 02:03:08 +0530

    Create model.py

---
+++ Backend/model.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+
+# Load model once (VERY IMPORTANT)
+model = YOLO("weights/best.pt")  # your trained model
+
+def detect_objects(image):
+    results = model(image)
+
+    detected = {
+        "smoke": False,
+        "fire": False
+    }
+
+    for r in results:
+        for box in r.boxes:
+            cls = int(box.cls[0])
+            label = model.names[cls]
+
+            if label == "smoke":
+                detected["smoke"] = True
+            if label == "fire":
+                detected["fire"] = True
+
+    return detected
